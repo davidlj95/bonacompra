c50f298fb7e816c2fe9cc3a50349f8b7f0a6093e
David LJ <mail@davidlj95.com>
2025-06-14 19:30:11 +0200

    build: initial commit
    app generated with
    spring init -d devtools,web --description Automate your groceries as much as possible -g com.davidlj95 -j 23 -l kotlin -t gradle-project-kotlin bonacompra

---
+++ src/test/kotlin/com/davidlj95/bonacompra/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.davidlj95.bonacompra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
