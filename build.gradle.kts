f32dec22ac8f792920f9d41242ae9c3e2b15a9f0
David LJ <mail@davidlj95.com>
2025-06-16 22:17:28 +0200

    feat: generate OpenAPI from code using plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 }
 
 group = "com.davidlj95"
